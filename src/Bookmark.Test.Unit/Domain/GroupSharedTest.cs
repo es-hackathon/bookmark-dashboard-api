d425a759ed50b305bae74aa11a202cb2a1344716
Amit P Naik <amit.naik8103@gmail.com>
2020-09-13 13:05:44 +0530

    test: domain class group shared

---
+++ src/Bookmark.Test.Unit/Domain/GroupSharedTest.cs
@@ -0,0 +1,29 @@
+﻿using Bookmark.Domain.Entities;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookmark.Test.Unit.Domain
+{
+    public class GroupSharedTest
+    {
+        private readonly GroupShared _groupShared;
+        private readonly int Id=1;
+
+        public GroupSharedTest()
+        {
+            _groupShared = new GroupShared();
+        }
+
+        [Test]
+        public void TestSetAndGetId()
+        {
+            _groupShared.Id = Id;
+            Assert.That(_groupShared.Id, Is.EqualTo(Id));
+        }
+       
+    }
+}
