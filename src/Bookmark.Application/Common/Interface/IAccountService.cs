509b6a1f5352d389a5470d515df673655a0825c7
Amit P Naik <amit.naik8103@gmail.com>
2020-09-13 17:59:08 +0530

    feat: added account service

---
+++ src/Bookmark.Application/Common/Interface/IAccountService.cs
@@ -0,0 +1,19 @@
+﻿using Bookmark.Domain.Auth;
+using Bookmark.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookmark.Application.Common.Interface
+{
+    public interface IAccountService
+    {
+        Task<Response<AuthenticationResponse>> AuthenticateAsync(AuthenticationRequest request, string ipAddress);
+        Task<Response<string>> RegisterAsync(RegisterRequest request, string origin);
+        Task<Response<string>> ConfirmEmailAsync(string userId, string code);
+        Task ForgotPassword(ForgotPasswordRequest model, string origin);
+        Task<Response<string>> ResetPassword(ResetPasswordRequest model);
+    }
+}
