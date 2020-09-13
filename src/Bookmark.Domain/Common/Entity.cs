3508992d363974d42797e47103dd0925f8c21b16
Amit P Naik <amit.naik8103@gmail.com>
2020-09-13 13:08:22 +0530

    feat: added entities in domain layer

---
+++ src/Bookmark.Domain/Common/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookmark.Domain.Common
+{
+    public abstract class Entity<TKey> : IHasKey<TKey>, ITrackable
+    {
+        public TKey Id { get; set; }
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+    }
+}
---
+++ src/Bookmark.Domain/Entities/Groups.cs
@@ -0,0 +1,16 @@
+﻿using Bookmark.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookmark.Domain.Entities
+{
+    public class Groups : AggregateRoot<int>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
