c73cccaed8e67c822721597977a3898d6d57ae24
Amit P Naik <amit.naik8103@gmail.com>
2020-09-13 20:46:30 +0530

    feat: added swagger

--- src/Bookmark.WebApi/Startup.cs
+++ src/Bookmark.WebApi/Startup.cs
@@ -1,14 +1,22 @@
 using Bookmark.Common;
 using Bookmark.Infrastructure;
 using Bookmark.Infrastructure.Services;
+using Bookmark.Application.Common.Interface;
 using Bookmark.Persistance;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Serilog;
+using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.OpenApi.Models;
+using System.Collections.Generic;
 
 namespace Bookmark.WebApi
 {
@@ -30,17 +38,87 @@ namespace Bookmark.WebApi
                 opt.UseSqlServer(Configuration.GetConnectionString("BookmarkConn")
           ));
 
+ 
             services.AddIdentityInfrastructure(Configuration);
             services.AddTransient<IEmailService, MailService>();
       
+ 
+            services.AddSwaggerGen(setupAction =>
+            {
+                setupAction.SwaggerDoc(
+                    "OpenAPISpecification",
+                    new Microsoft.OpenApi.Models.OpenApiInfo()
+                    {
+                        Title = "Bookmark Dashboard Web API",
+                        Version = "1",
+                        Description = "Web API solution template which is built on Clean Architecture using .Net Core for bookmark dashboard project",
+                        Contact = new OpenApiContact()
+                        {
+                            Email = "amit.naik8103@gmail.com",
+                            Name = "Amit Naik",
+                            Url = new Uri("https://amitpnk.github.io/")
+                        },
+                        License = new OpenApiLicense()
+                        {
+                            Name = "MIT License",
+                            Url = new Uri("https://opensource.org/licenses/MIT")
+                        }
+                    });
+                    setupAction.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                    {
+                        Type = SecuritySchemeType.Http,
+                        Scheme = "bearer",
+                        BearerFormat = "JWT",
+                        Description = $"Input your Bearer token in this format - Bearer token to access this API",
+                    });
+                    setupAction.AddSecurityRequirement(new OpenApiSecurityRequirement
+                    {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer",
+                                },
+                            }, new List<string>()
+                        },
+                    });
+                var xmlCommentsFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFile);
+                setupAction.IncludeXmlComments(xmlCommentsFullPath);
+            });
+
+            services.AddScoped<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
+
+            services.AddApiVersioning(config =>
+            {
+                config.DefaultApiVersion = new ApiVersion(1, 0);
+                config.AssumeDefaultVersionWhenUnspecified = true;
+                config.ReportApiVersions = true;
+            });
+
+ 
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory log)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            log.AddSerilog();
+
+            //app.UseMiddleware<CustomExceptionHandlerMiddleware>();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(setupAction =>
+            {
+                setupAction.SwaggerEndpoint("/swagger/OpenAPISpecification/swagger.json", "Clean Architecture WebAPI");
+                setupAction.RoutePrefix = "OpenAPI";
+            });
 
             app.UseHttpsRedirection();
 
