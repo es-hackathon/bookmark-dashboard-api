33fbad82994628c831fb77d5dfe85de17dd545a3
Amit P Naik <amit.naik8103@gmail.com>
2020-09-13 15:24:50 +0530

    test: db context file

---
+++ src/Bookmark.Test.Unit/Persistence/ApplicationDbContextTest.cs
@@ -0,0 +1,33 @@
+﻿using Bookmark.Domain.Entities;
+using Bookmark.Persistance;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookmark.Test.Unit.Persistence
+{
+    public class ApplicationDbContextTest
+    {
+        [Test]
+        public void CanInsertCardIntoDatabasee()
+        {
+            using var context = new ApplicationDbContext();
+            var cards = new Cards();
+            context.Cards.Add(cards);
+            Assert.AreEqual(EntityState.Added, context.Entry(cards).State);
+        }
+
+        [Test]
+        public void CanInsertGroupIntoDatabasee()
+        {
+            using var context = new ApplicationDbContext();
+            var groups = new Groups();
+            context.Groups.Add(groups);
+            Assert.AreEqual(EntityState.Added, context.Entry(groups).State);
+        }
+    }
+}
