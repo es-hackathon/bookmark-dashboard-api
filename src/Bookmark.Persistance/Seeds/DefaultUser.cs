6aa14f235e800168abc44915b71f36fab0f13e42
Amit P Naik <amit.naik8103@gmail.com>
2020-09-13 16:59:20 +0530

    feat: data seeding file for user and role

---
+++ src/Bookmark.Persistance/Seeds/DefaultUser.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Bookmark.Domain.Auth;
+using System.Collections.Generic;
+using Bookmark.Domain.Enum;
+
+namespace Bookmark.Persistence.Seeds
+{
+    public static class DefaultUser
+    {
+        public static List<ApplicationUser> IdentityBasicUserList()
+        {
+            return new List<ApplicationUser>()
+            {
+                new ApplicationUser
+                {
+                    Id = Constants.SuperAdminUser,
+                    UserName = "superadmin",
+                    Email = "superadmin@gmail.com",
+                    FirstName = "Amit",
+                    LastName = "Naik",
+                    EmailConfirmed = true,
+                    PhoneNumberConfirmed = true,
+                    PasswordHash = "Password@123",
+                    NormalizedEmail= "SUPERADMIN@GMAIL.COM",
+                    NormalizedUserName="SUPERADMIN"
+                },
+                new ApplicationUser
+                {
+                    Id = Constants.BasicUser,
+                    UserName = "basicuser",
+                    Email = "basicuser@gmail.com",
+                    FirstName = "Basic",
+                    LastName = "User",
+                    EmailConfirmed = true,
+                    PhoneNumberConfirmed = true,
+                    PasswordHash = "Password@123",
+                    NormalizedEmail= "BASICUSER@GMAIL.COM",
+                    NormalizedUserName="BASICUSER"
+                },
+            };
+        }
+    }
+}
