97e1c6863850e9cd478faafd002a1fc7fd0ba1da
Amit P Naik <amit.naik8103@gmail.com>
2020-09-13 13:07:43 +0530

    test: domain class card favorite

---
+++ src/Bookmark.Test.Unit/Domain/CardFavoriteTest.cs
@@ -0,0 +1,28 @@
+﻿using Bookmark.Domain.Entities;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookmark.Test.Unit.Domain
+{
+    public class CardFavoriteTest
+    {
+        private readonly CardFavorite _cardFavorite;
+        private readonly int Id = 1;
+
+        public CardFavoriteTest()
+        {
+            _cardFavorite = new CardFavorite();
+        }
+
+        [Test]
+        public void TestSetAndGetId()
+        {
+            _cardFavorite.Id = Id;
+            Assert.That(_cardFavorite.Id, Is.EqualTo(Id));
+        }
+    }
+}
