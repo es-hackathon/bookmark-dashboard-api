4eae3e347de936ec3f6e1d8a17dab71a28b46e26
Amit P Naik <amit.naik8103@gmail.com>
2020-09-14 10:37:06 +0530

    feat: CQRS for delete card

---
+++ src/Bookmark.Application/Features/CardFeature/Commands/DeleteCard/DeleteCardCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using System;
+
+namespace Bookmark.Application.Features.CardFeature.Commands.DeleteCard
+{
+    public class DeleteCardCommand : IRequest
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ src/Bookmark.Application/Features/CardFeature/Commands/DeleteCard/DeleteCardCommandHandler.cs
@@ -0,0 +1,56 @@
+﻿using Bookmark.Application.Common.Exceptions;
+using Bookmark.Application.Common.Interface;
+using Bookmark.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore.Internal;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bookmark.Application.Features.CardFeature.Commands.DeleteCard
+{
+    public class DeleteCardCommandHandler : IRequestHandler<DeleteCardCommand>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public DeleteCardCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<Unit> Handle(DeleteCardCommand request, CancellationToken cancellationToken)
+        {
+            DeleteCardCommandValidator validator = new DeleteCardCommandValidator();
+
+            var results = validator.Validate(request);
+
+            if (!results.IsValid)
+            {
+                throw new ValidationException(results.ToString("~"));
+            }
+
+            var entity = await _context.Cards
+                .FindAsync(request.Id);
+
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(Cards), request.Id);
+            }
+
+            //var hasRecord = _context.Sales.Any(o => o.CardId == entity.Id);
+            //if (hasRecord)
+            //{
+            //    throw new DeleteFailureException(nameof(Cards), request.Id, "There are existing customer id associated with this Sales");
+            //}
+
+            _context.Cards.Remove(entity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+
+
+    }
+}
